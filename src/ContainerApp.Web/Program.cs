f7435be70de0f2a6d096492a329b26e6adeb45b6
FrederikBA <frederikbandersen98@gmail.com>
2023-11-12 20:21:37 +0100

    docker-compose

--- src/ContainerApp.Web/Program.cs
+++ src/ContainerApp.Web/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var policyName = "AllowOrigin";
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+        builder =>
+        {
+            builder
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+app.MapGet("/", () => "Hello World! I am now changed!");
+
+app.UseCors(policyName);
+
 
 app.Run();
